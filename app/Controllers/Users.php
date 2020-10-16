0806c72165f8108e6c155dfe78e4acc257df46bd
alankurniadi12 <alankurniadi53@gmail.com>
2020-10-16 14:26:19 +0700

    upload first

---
+++ app/Controllers/Users.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controllers;
+
+use CodeIgniter\RESTful\ResourceController;
+
+class Users extends ResourceController {
+
+    protected $modelName = 'App\Models\UsersModel';
+    protected $format = 'json';
+
+    public function __construct()
+    {
+        $this->validation = \Config\Services::validation();
+    }
+
+    public function index() {
+        return $this->respond($this->model->findAll());
+    }
+
+    public function create()
+    {
+        $data = $this->request->getPost();
+        $validate = $this->validation->run($data, 'register');
+        $errors = $this->validation->getErrors();
+
+        if($errors){
+            return $this->fail($errors);
+        }
+
+        $user = new \App\Entities\Users();
+        $user->fill($data);
+        $user->created_by = 0;
+        $user->created_date = date("Y-m-d H:i:s");
+
+        if($this->model->save($user)) 
+        {
+            return $this->respondCreated($user, 'user created');
+        }
+    }
+}
